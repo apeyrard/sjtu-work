6b76f2fac5f05b9849223d0847a5988dc6f575f3
Alex Peyrard <alex.peyrard@gmail.com>
2014-11-19 15:41:15 +0800

    ex 4 ok

--- DIP/exercises/ex4/filtering.py
+++ DIP/exercises/ex4/filtering.py
@@ -4,6 +4,7 @@
 import argparse
 import sys
 import math
+import statistics
 from PIL import Image
 import numpy as np
 
@@ -99,6 +100,57 @@ def contraMean(matrix, x, y, m, n, order):
     else:
         return (tmp/tmp2)*255
 
+def statFilter(matrix, x, y, m, n, aux):
+    if m%2 ==0 or n%2==0:
+        print("must be odd")
+    else:
+        m = math.floor(m/2)
+        n = math.floor(n/2)
+        tmpList = []
+        for j in range(-m,m+1):
+            for k in range(-n,n+1):
+                try:
+                    if x+j < 0 or y+k < 0:
+                        raise IndexError()
+                    tmpList.append(matrix[x+j][y+k])
+                except IndexError:
+                    pass
+    if aux == 'median':
+        return statistics.median(tmpList)
+    elif aux == 'max':
+        return max(tmpList)
+    elif aux == 'min':
+        return min(tmpList)
+    elif aux == 'midpoint':
+        return 0.5*(max(tmpList) + min(tmpList))
+
+def alpha(matrix, x, y, m, n, d):
+    if m%2 ==0 or n%2==0 or d%2==1:
+        print("m,n must be odd")
+        print("d must be even")
+    else:
+        m = math.floor(m/2)
+        n = math.floor(n/2)
+        tmpList = []
+        for j in range(-m,m+1):
+            for k in range(-n,n+1):
+                try:
+                    if x+j < 0 or y+k < 0:
+                        raise IndexError()
+                    tmpList.append(matrix[x+j][y+k])
+                except IndexError:
+                    pass
+        z = int(d/2)
+        nb = len(tmpList)
+        trimmedList = sorted(tmpList)[z:nb-z]
+        if trimmedList:
+            result = sum(trimmedList)/(nb-d)
+        else:
+            result = 0
+        return result
+
+
+
 def filter(matrix, function, *args):
     newMat = matrix.copy()
     for y in range(newMat.shape[1]):
@@ -123,6 +175,20 @@ elif args.filter == 'harmo':
     function = harmoMean
 elif args.filter == 'contra':
     function = contraMean
+elif args.filter == 'median':
+    function = statFilter
+    aux = 'median'
+elif args.filter == 'max':
+    function = statFilter
+    aux = 'max'
+elif args.filter == 'min':
+    function = statFilter
+    aux = 'min'
+elif args.filter == 'midpoint':
+    function = statFilter
+    aux = 'midpoint'
+elif args.filter == 'alpha':
+    function = alpha
 
 try:
     im = Image.open(args.image)
@@ -131,7 +197,7 @@ except FileNotFoundError as e:
 
 matrix = getMatrix(im)
 
-matrix = filter(matrix, function, 3, 3, -2)
+matrix = filter(matrix, function, 3, 3, aux)
 
 newim = Image.new(im.mode, im.size)
 newim.putdata(getData(matrix))
