81f2fa0532cd36275efde16d2f4842f554573c3e
Alex Peyrard <alex.peyrard@gmail.com>
2014-11-19 15:56:10 +0800

    started ex5

---
+++ DIP/exercises/ex5/ex5.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import sys
+
+from PIL import Image
+import numpy as np
+
+def getMatrix(image):
+    data = list(image.getdata())
+    width, height = image.size
+    matrix = np.array(data).reshape(height,width)
+    return matrix
+
+def getData(matrix):
+    data = list(matrix.reshape(matrix.shape[0]*matrix.shape[1]))
+    return data
+
+def preprocessing(matrix):
+    newMat = matrix.copy()
+    for y in range(newMat.shape[1]):
+        for x in range(newMat.shape[0]):
+            newMat[x][y] = newMat[x][y]*(-1)**(x+y)
+    return newMat
+
+def postprocessing(matrix):
+    return preprocessing(matrix)
+
+def blur(matrix, a, b, T):
+    print(matrix)
+
+try:
+    im = Image.open("./book_cover.jpg")
+except FileNotFoundError as e:
+    sys.exit("Error : file not found")
+
+matrix = getMatrix(im)
+prepMat = preprocessing(matrix)
+fourierMat = np.fft.fft2(prepMat)
+
+blurredMat = blur(fourierMat, a=0.1, b=0.1, T=1)
+
+
+#blurred = Image.new(im.mode, im.size)
+
+
+#gaussHigh = np.fft.ifft2(gaussHighF)
+
+#postGaussHigh = postprocessing(gaussHigh)
+
+#imGaussHigh.putdata(getData(postGaussHigh))
+
+#imGaussHigh.show()
+
+
